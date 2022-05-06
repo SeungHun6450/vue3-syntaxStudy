02b264f5fa648aead941ecd6384bd8e2707cfd7a
SeungHun6450 <toffg6450@naver.com>
2022-05-06 17:49:54 +0900

    study syntax

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+<!-- v-once -->
+  <!-- <h1
+    v-once
+    @click="add">
+    {{ msg }}
+  </h1> -->
+
+  <!-- v-html -->
+  <!-- <h1 v-html="msg"></h1> -->
+
+  <!-- v-bind  -->
+  <!-- <h1 :class="msg"> -->
+  <!-- 
+  <h1 v-bind:class="msg">
+    {{ msg }}
+  </h1> -->
+
+  <!-- 동적 전달인자 -->
+  <h1 
+    :[attr]="'active'"
+    @[event]="add">
+    {{ msg }}
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // v-once
+      // msg: 'Hello world!!'
+
+      // v-html
+      // msg: '<div style="color: red;">Hello!!</div>'
+
+      // v-bind
+      // msg: 'active'
+
+      // 동적 전달인자
+      msg: 'active',
+      attr: 'class',
+      event: 'click'
+    }
+  },
+  methods: {
+    add() {
+      this.msg += '!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .active {
+    color: royalblue;
+    font-size: 100px;
+  }
+</style>
+
